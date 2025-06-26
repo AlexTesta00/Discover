ebc33e1357d3da907704cf079ac8d54cc8ff48d2
Alex Testa <alex.testa@studio.unibo.it>
2025-06-26 16:13:35 +0200

    chore: disable debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@ import 'package:discover/features/authentication/presentation/pages/authenticati
 import 'package:discover/features/onboarding/presentation/pages/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Supabase.initialize(
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2YXZkaWJwYXJid2d1dWlmdHJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2ODg0OTMsImV4cCI6MjA2NjI2NDQ5M30.Wx4ti4pmTMa7dVVROEMIwDMgQVfQD6EbThj7fA-S-pQ',
+    url: 'https://xvavdibparbwguuiftrs.supabase.co',
+  );
   final sharedPreferences = await SharedPreferences.getInstance();
   final isFirstRun = sharedPreferences.getBool('onBoardingComplete') ?? true;
 
@@ -22,6 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: AppTheme.lightTheme,
       home: isFirstRun ? const OnBoardingScreen() : const AuthenticationPage(),

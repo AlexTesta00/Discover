e7c9a0a82bbbb0eb8691fcb057169ac5778750f0
Alex Testa <alex.testa@studio.unibo.it>
2025-06-26 16:54:08 +0200

    feat: hide supbase key in .env file

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,16 @@ import 'package:discover/config/themes/app_theme.dart';
 import 'package:discover/features/authentication/presentation/pages/authentication_page.dart';
 import 'package:discover/features/onboarding/presentation/pages/onboarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Supabase.initialize(
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2YXZkaWJwYXJid2d1dWlmdHJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2ODg0OTMsImV4cCI6MjA2NjI2NDQ5M30.Wx4ti4pmTMa7dVVROEMIwDMgQVfQD6EbThj7fA-S-pQ',
-    url: 'https://xvavdibparbwguuiftrs.supabase.co',
+    anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
+    url: dotenv.env['SUPABASE_URL'] ?? '',
   );
   final sharedPreferences = await SharedPreferences.getInstance();
   final isFirstRun = sharedPreferences.getBool('onBoardingComplete') ?? true;

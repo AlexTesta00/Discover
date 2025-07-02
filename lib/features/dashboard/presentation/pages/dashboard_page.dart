f43c97d58dbc6eec3ecc9abd25112f2980bb98fe
Alex Testa <alex.testa@studio.unibo.it>
2025-07-02 13:04:57 +0200

    feat: added bottom nav bar

--- lib/features/dashboard/presentation/pages/dashboard_page.dart
+++ lib/features/dashboard/presentation/pages/dashboard_page.dart
@@ -1,5 +1,7 @@
 import 'package:discover/features/authentication/domain/use_cases/authentication_service.dart';
+import 'package:discover/features/maps/presentation/pages/itinerary_page.dart';
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -35,11 +37,29 @@ class _DashboardPageState extends State<DashboardPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dashboard'),
+        title: const Text('Dashboard', style: TextStyle(color: Colors.black),),
+        centerTitle: true,
+        iconTheme: const IconThemeData(color: Colors.black),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         actions: [
           IconButton(onPressed: logout, icon: const Icon(Icons.logout)),
         ],
       ),
+      body: PersistentTabView(
+        tabs: [
+          PersistentTabConfig(
+            screen: const ItineraryPage(), 
+            item: ItemConfig(
+                icon: Icon(Icons.map),
+                title: 'Itinerario',
+              )
+            )
+        ], 
+        navBarBuilder: (navBarConfig) => Style2BottomNavBar(
+          navBarConfig: navBarConfig
+          ),
+        ),
     );
   }
 }
---
+++ lib/features/maps/presentation/pages/itinerary_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ItineraryPage extends StatefulWidget {
+  const ItineraryPage({super.key});
+
+  @override
+  State<ItineraryPage> createState() => _ItineraryPageState();
+}
+
+class _ItineraryPageState extends State<ItineraryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue
+    );
+  }
+}

8a41ad8e8b2e8f1deb372553b1259227ee077bfd
Alex Testa <alex.testa@studio.unibo.it>
2025-08-26 19:06:35 +0200

    fix(ci): fix bug that not generate debug apk

--- release.config.js
+++ release.config.js
@@ -3,16 +3,10 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/github',
-      {
-        assets: [
-          {
-            path: 'build/app/outputs/flutter-apk/app-debug.apk',
-            label: 'Android Debug APK'
-          }
-        ]
-      }
-    ]
+    ['@semantic-release/github', {
+      assets: [
+        { path: 'build/app/outputs/**/*app-*.apk', label: 'Android APK' }
+      ]
+    }]
   ]
 }

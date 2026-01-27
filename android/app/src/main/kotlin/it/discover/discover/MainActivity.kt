5b89c7b7f06d1ed86d814931e9e3303af560ea40
Alex Testa <alex.testa@studio.unibo.it>
2026-01-27 16:38:55 +0100

    chore: change app icon

--- android/app/src/main/kotlin/it/discover/discover/MainActivity.kt
+++ android/app/src/main/kotlin/it/discover/discover/MainActivity.kt
@@ -1,5 +1,25 @@
 package it.discover.discover
 
+import android.app.ActivityManager
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val taskDescription = ActivityManager.TaskDescription.Builder()
+                .setPrimaryColor(Color.WHITE)   // Colore sfondo Recents
+                .build()
+
+            setTaskDescription(taskDescription)
+        } else {
+            @Suppress("DEPRECATION")
+            setTaskDescription(ActivityManager.TaskDescription(null, null, Color.WHITE))
+        }
+    }
+}
